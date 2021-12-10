141090de9c5a234dd0f4c5f0f5e0023b0fad8382
mihirsamdarshi <msamdars@stanford.edu>
2021-12-10 14:52:56 -0800

    fix: lint pipeline scripts

--- scripts/pipeline_job_summary.py
+++ scripts/pipeline_job_summary.py
@@ -1,27 +1,44 @@
-
 import argparse
-import getopt
-from google.cloud import storage
 import json
-import os
-import pathlib
-import re
-import sys
 import warnings
+
 import dateparser
-warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
+from google.cloud import storage
+
+
+warnings.filterwarnings(
+    "ignore", "Your application has authenticated using end user credentials"
+)
+
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='Calculate a job completion time')
-    parser.add_argument('-p', '--project', required=True, type=str,
-                        help='GCP project name')
-    parser.add_argument('-b', '--bucket_origin', required=True, type=str,
-                        help='Bucket with output files')
-    parser.add_argument('-r', '--results_folder', required=True, type=str,
-                        help='Path to the results folder')
-    parser.add_argument('-i', '--caper_job_id', required=True, type=str,
-                        help='Caper job id (E.g.: 9c6ff6fe-ce7d-4d23-ac18-9935614d6f9b)')
+    parser.add_argument(
+        '-p', '--project', required=True, type=str, help='GCP project name'
+    )
+    parser.add_argument(
+        '-b',
+        '--bucket_origin',
+        required=True,
+        type=str,
+        help='Bucket with output files',
+    )
+    parser.add_argument(
+        '-r',
+        '--results_folder',
+        required=True,
+        type=str,
+        help='Path to the results folder',
+    )
+    parser.add_argument(
+        '-i',
+        '--caper_job_id',
+        required=True,
+        type=str,
+        help='Caper job id (E.g.: 9c6ff6fe-ce7d-4d23-ac18-9935614d6f9b)',
+    )
     return parser
+
 
 def main():
     parser = arg_parser()
@@ -37,7 +54,7 @@ def main():
     print('Results folder:', results_folder)
 
     caper_job_id = args.caper_job_id
-    print('Caper Job ID:',caper_job_id)
+    print('Caper Job ID:', caper_job_id)
 
     storage_client = storage.Client(project_name)
     # bucket_source = storage_client.get_bucket(bucket_origin)
@@ -52,11 +69,11 @@ def main():
             print('\nMetadata file location:\n', filename, '\n')
             metadata = json.loads(blob.download_as_string(client=None))
             break
-    
+
     if metadata.get('start'):
         start_time = dateparser.parse(metadata['start'])
         end_time = dateparser.parse(metadata['end'])
-        print('Pipeline Running Time: ',end_time - start_time)
+        print('Pipeline Running Time: ', end_time - start_time)
     else:
         print('\nStart time not available!!!\n')
 
@@ -67,7 +84,7 @@ def main():
             causeby_len = len(metadata['failures'][x]['causedBy'])
             for y in range(causeby_len):
                 output = metadata['failures'][x]['causedBy'][y]['message']
-                print('\t- MESSAGE ', y+1,': ', output,"\n")
+                print('\t- MESSAGE ', y + 1, ': ', output, "\n")
     else:
         print('+ No errors detected (congratulations)!\n')
 
